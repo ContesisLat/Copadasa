32d0b4835328b9ef4be70194f0f36baddb77c187
ContesisLat <contesispty@gmail.com>
2024-05-11 12:45:07 -0600

    mutiples Popups

--- Vue/copadasa/src/store/PopupStore.ts
+++ Vue/copadasa/src/store/PopupStore.ts
@@ -2,17 +2,23 @@ import { defineStore } from 'pinia';
 
 export const usePopupStore = defineStore('popup', {
   state: () => ({
-    isPopupOpen: false,
     popupComponentName: "",
+    openedPopups: [] as { componentName: string, width: number, height: number, posX: number, posY: number }[],
   }),
   actions: {
     openPopup(componentName: string) {
+      this.openedPopups.push({
+        componentName: componentName,
+        width: 800, // Valor predeterminado para el ancho
+        height: 500, // Valor predeterminado para el alto
+        posX: 0, // Valor predeterminado para la posición X
+        posY: 0, // Valor predeterminado para la posición Y
+      });
       this.popupComponentName = componentName;
-      this.isPopupOpen = true;
     },
-    closePopup() {
-      this.popupComponentName = "";
-      this.isPopupOpen = false;
+    closePopup(index:number) {
+      // Implementación del cierre de la ventana emergente
+      this.openedPopups.splice(index, 1);
     },
   },
 });
--- Vue/copadasa/src/views/PopupWindow.vue
+++ Vue/copadasa/src/views/PopupWindow.vue
@@ -1,132 +1,152 @@
 <template>
-    <div 
-      class="popup-window" v-if="store.isPopupOpen"
-      :style="{ top: posY + 'px', left: posX + 'px', width: width + 'px', height: height + 'px' }"
-      @mousedown="startDrag"
-    >
-      <div class="content"> 
-        <!-- Contenido de la ventana emergente -->
-        <component :is="currentComponent"/>
-      </div>
-      <button type="button" class="close-btn" aria-label="Close" @click="store.closePopup"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-</svg></button>
-      <!-- Botón para redimensionar -->
-      <button class="resize-btn" @mousedown="startResize"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fullscreen" viewBox="0 0 16 16">
-  <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5M.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5"/>
-</svg></button>
+  <div class="popup-window" v-for="(popup, index) in openedPopups" :key="index"
+    :style="{ top: popup.posY + 'px', left: popup.posX + 'px', width: popup.width + 'px', height: popup.height + 'px' }"
+    @mousedown="startDrag($event, index)">
+    <div class="content">
+      <!-- Contenido de la ventana emergente -->
+      <component :is="getComponentName(index)" />
     </div>
-  </template>
-  
-  <script lang="ts" setup>
-  import {usePopupStore} from "@/store/PopupStore"
-  import globalComponents from "@/store/global-component";
-  import { ref,onMounted, computed } from 'vue';
-  const width = ref(800);
-  const height = ref(500);
-  const posX = ref(0);  
-  const posY = ref(0); 
-  const startX = ref(0);
-  const startY = ref(0);
-  const isDragging = ref(false);
-  const store = usePopupStore()
-  const currentComponent = computed(() => {
-  const componentName = store.popupComponentName;
-  return globalComponents[componentName];
-});
+    <button type="button" class="close-btn" aria-label="Close" @click="store.closePopup(index)">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg"
+        viewBox="0 0 16 16">
+        <path
+          d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+      </svg>
+    </button>
+    <!-- Botón para redimensionar -->
+    <button class="resize-btn" @mousedown="startResize($event, index)">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fullscreen"
+        viewBox="0 0 16 16">
+        <path
+          d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5M.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5" />
+      </svg>
+    </button>
+  </div>
+</template>
 
+<script setup>
+import { usePopupStore } from "@/store/PopupStore";
+import globalComponents from "@/store/global-component";
+import { ref, computed, watch } from 'vue';
 
-  const centerPopup = () => {
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight;
+const store = usePopupStore();
+const openedPopups = computed(() => store.openedPopups);
 
-  posX.value = (windowWidth - width.value) / 2;
-  posY.value = (windowHeight - height.value) / 2;
-};
+// para indicar si se está redimensionando
+let isResizing = false;
 
-onMounted(centerPopup);
-  
+const startDrag = (event, index) => {
+  if (!event || !event.clientX || !event.clientY || isResizing) return;
+  const popup = openedPopups.value[index];
+  const startX = event.clientX - popup.posX;
+  const startY = event.clientY - popup.posY;
 
-  const startDrag = (event: MouseEvent) => {
-    if (!event.target.classList.contains('resize-btn')) {
-      isDragging.value = true;
-      startX.value = event.clientX - posX.value;
-      startY.value = event.clientY - posY.value;
-      document.addEventListener('mousemove', handleDrag);
-      document.addEventListener('mouseup', stopDrag);
-    }
+  const handleDrag = (event) => {
+    if (!event.clientX || !event.clientY) return;
+    popup.posX = event.clientX - startX;
+    popup.posY = event.clientY - startY;
   };
-  
-  const handleDrag = (event: MouseEvent) => {
-    if (isDragging.value) {
-      posX.value = event.clientX - startX.value;
-      posY.value = event.clientY - startY.value;
-    }
-  };
-  
+
   const stopDrag = () => {
-    isDragging.value = false;
     document.removeEventListener('mousemove', handleDrag);
     document.removeEventListener('mouseup', stopDrag);
   };
-  
-  const startResize = (event: MouseEvent) => {
-    let startX = event.clientX;
-    let startY = event.clientY;
-  
-    const handleResize = (event: MouseEvent) => {
-      width.value = Math.max(100, width.value + event.clientX - startX);
-      height.value = Math.max(100, height.value + event.clientY - startY);
-      startX = event.clientX;
-      startY = event.clientY;
-    };
-  
-    const stopResize = () => {
-      document.removeEventListener('mousemove', handleResize);
-      document.removeEventListener('mouseup', stopResize);
-    };
-  
-    document.addEventListener('mousemove', handleResize);
-    document.addEventListener('mouseup', stopResize);
+
+  document.addEventListener('mousemove', handleDrag);
+  document.addEventListener('mouseup', stopDrag);
+};
+
+const startResize = (event, index) => {
+  if (!event || !event.clientX || !event.clientY) return;
+  const popup = openedPopups.value[index];
+  let startX = event.clientX;
+  let startY = event.clientY;
+
+  // Cambiar a true durante la operación de redimensionamiento
+  isResizing = true;
+
+  const handleResize = (event) => {
+    if (!event.clientX || !event.clientY) return;
+    popup.width = Math.max(100, popup.width + event.clientX - startX);
+    popup.height = Math.max(100, popup.height + event.clientY - startY);
+    startX = event.clientX;
+    startY = event.clientY;
   };
-  
-  </script>
-  
-  <style scoped>
-  .popup-window {
-    position: absolute;
-    background-color: transparent;
-    border: 2px solid #ccc;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    overflow: hidden;
+
+  const stopResize = () => {
+    document.removeEventListener('mousemove', handleResize);
+    document.removeEventListener('mouseup', stopResize);
+    // Restaurar a false al finalizar la operación de redimensionamiento
+    isResizing = false;
+  };
+
+  document.addEventListener('mousemove', handleResize);
+  document.addEventListener('mouseup', stopResize);
+};
+
+const getComponentName = (index) => {
+  const componentName = openedPopups.value[index].componentName;
+  return globalComponents[componentName];
+};
+
+const centerPopup = (index) => {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  const popup = openedPopups.value[index];
+  if (popup) {
+    popup.posX = (windowWidth - popup.width) / 2;
+    popup.posY = (windowHeight - popup.height) / 2;
   }
-  
-  .content {
-    position: relative;
-    width: 100%;
-    height: 100%;
+};
+
+let previousLength = 0;
+
+watch(() => openedPopups.value.length, (newValue) => {
+  if (newValue > previousLength) {
+    const lastIndex = newValue - 1; // Índice del último popup agregado
+    centerPopup(lastIndex);
   }
-  
-  .close-btn {
-    position: absolute;
-    top: 5px;
-    right: 5px;
-    color: black;
-    border: none;
-    cursor: pointer;
-    background-color: transparent;
-  }
-  
-  .resize-btn {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    padding: 5px 10px;
-    background-color: darkblue;
-    color: white;
-    border: none;
-    cursor: pointer;
-  }
-  </style>
+  previousLength = newValue; // Actualizar el valor anterior
+});
+
+</script>
+
+<style scoped>
+.popup-window {
+  position: absolute;
+  background-color: transparent;
+  border: 2px solid #ccc;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+}
+
+.content {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.close-btn {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  color: black;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+}
+
+.resize-btn {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 5px 10px;
+  background-color: darkblue;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
   
   
