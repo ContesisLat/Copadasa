1e31a1e2875b858865f25e347839d9381e3aa3fe
ContesisLat <contesispty@gmail.com>
2025-11-25 15:59:28 -0600

    actualizacion 11/25/2025

--- Vue/ContesisLat/src/views/EstructuraS/SideBar.vue
+++ Vue/ContesisLat/src/views/EstructuraS/SideBar.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <!-- Botón para abrir el menú -->
   <a class="btn" @click="cargarProgramas(perfil)" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
     aria-controls="offcanvasExample" style="color: white; border: none; border-radius: 5px;">
@@ -9,37 +8,23 @@
   <!-- Panel lateral -->
   <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
 
-=======
-  <a class="btn" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"
-    style="color: white;">
-    ☰
-  </a>
-
-  <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel"
-    style="font-family: Trebuchet MS;">
->>>>>>> 25c1d2ded1cd92aad99f2bf0cb6f741a09d0a518
     <div class="offcanvas-header">
       <h3>Estrategia Cargo</h3>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
+
     <div class="offcanvas-body d-flex flex-column gap-2">
-<<<<<<< HEAD
 
       <!-- Iteramos sobre los programas -->
       <div class="btn-group dropend" v-for="(programa, index) in programas" :key="index">
 
         <!-- Nombre del programa -->
-=======
-      <div>
-        <h1>texto</h1>
-      </div>
-      <div class="btn-group dropend">
->>>>>>> 25c1d2ded1cd92aad99f2bf0cb6f741a09d0a518
         <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="#" aria-expanded="false">
-          Parámetros
+          {{ programa.proceso }}
         </a>
+
+        <!-- Subprogramas -->
         <ul class="dropdown-menu" style="cursor: pointer;">
-<<<<<<< HEAD
           <div class="dropdown-inner">
 
             <li v-for="(sub, i) in programa.subprogramas" :key="i" @click="store.openPopup(sub.programa)">
@@ -54,77 +39,6 @@
             </li>
 
           </div>
-=======
-          <li @click="store.openPopup('PcP')"><a class="dropdown-item">Países/Ciudades/Puertos</a></li>
-          <li @click="store.openPopup('CrmClte')"><a class="dropdown-item">Catálogo de Clientes</a></li>
-          <li @click="store.openPopup('CarNatur')"><a class="dropdown-item">Naturaleza de Carga</a></li><br>
-          <li @click="store.openPopup('TiTarifa')"><a class="dropdown-item">Definición de Tarifas</a></li>
-          <li @click="store.openPopup('CarTarMan')"><a class="dropdown-item">Tarifas por Manejo</a></li>
-          <li @click="store.openPopup('CarTarAlm')"><a class="dropdown-item">Tarifas por Almacenaje</a></li>
-          <li @click="store.openPopup('CarTari')"><a class="dropdown-item">Tarifas Refrigeración</a></li><br>
-
-          <li @click="store.openPopup('CarAtenVue')"><a class="dropdown-item">Cargos por Atención de Vuelos</a></li>
-          <li @click="store.openPopup('TarVue')"><a class="dropdown-item">Tarifa Atención de Vuelos</a></li>
-          <li @click="store.openPopup('CarCoaer')"><a class="dropdown-item">Compañías Aéreas</a></li><br>
-
-          <li @click="store.openPopup('LogAlma')"><a class="dropdown-item">Definición de Almacenes</a></li>
-          <li @click="store.openPopup('LogTral')"><a class="dropdown-item">Transacciones de Almaén</a></li>
-          <li @click="store.openPopup('CarInent')"><a class="dropdown-item">Indicadores de Entrega</a></li>
-          <li @click="store.openPopup('LogUbica')"><a class="dropdown-item">Ubicaciones en Almacén</a></li>
-
-          <li @click="store.openPopup('BdSeg001')"><a class="dropdown-item">bases de datos</a></li>
-
-          <li><a class="dropdown-item disabled" aria-disabled="true">Something else here</a></li>
-        </ul>
-      </div>
-      <div class="btn-group dropend">
-        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="#" aria-expanded="false">
-          Manifiestos
-
-        </a>
-        <ul class="dropdown-menu" style="cursor: pointer;">
-          <li @click="store.openPopup('RegMani')"><a class="dropdown-item">Registro Manifiestos</a></li>
-          <li @click="store.openPopup('CtrlManifiesto')"><a class="dropdown-item">Control de Manifiestos</a></li>
-          <li @click="store.openPopup('CtrlDespacho')"><a class="dropdown-item">Despachos</a></li>
-
-
-          <li><a class="dropdown-item disabled" aria-disabled="true">Something else here</a></li>
-        </ul>
-      </div>
-      <div class="btn-group dropend">
-        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="#" aria-expanded="false">
-          Aeronaves
-
-        </a>
-        <ul class="dropdown-menu" style="cursor: pointer;">
-          <li @click="store.openPopup('RegSerAero')"><a class="dropdown-item">Servicios a Aeronaves</a></li>
-
-          <li><a class="dropdown-item disabled" aria-disabled="true">Something else here</a></li>
-        </ul>
-      </div>
-      <div class="btn-group dropend">
-        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="#" aria-expanded="false">
-          Almacén Frío
-
-        </a>
-        <ul class="dropdown-menu" style="cursor: pointer;">
-          <li @click="store.openPopup('CtrlCuartofrio')"><a class="dropdown-item">Control Cuarto Frío</a></li>
-
-          <li><a class="dropdown-item disabled" aria-disabled="true">Something else here</a></li>
-        </ul>
-      </div>
-      <div class="btn-group dropend">
-        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" href="#" aria-expanded="false">
-          Consulta/Reportes
-
-        </a>
-        <ul class="dropdown-menu" style="cursor: pointer;">
-          <li @click="store.openPopup('ExResSerAero')"><a class="dropdown-item">Servicios Aéreos</a></li>
-
-          <li><a class="dropdown-item disabled" aria-disabled="true">Something else here</a></li>
-
-
->>>>>>> 25c1d2ded1cd92aad99f2bf0cb6f741a09d0a518
         </ul>
 
       </div>
@@ -133,15 +47,41 @@
   </div>
 </template>
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 25c1d2ded1cd92aad99f2bf0cb6f741a09d0a518
 <script lang="ts" setup>
 import { usePopupStore } from '@/store/PopupStore';
+import { ref, watch, computed, onMounted } from 'vue'
+import axios from 'axios';
+import { UrlGlobal } from '@/store/dominioGlobal';
+import { userGlobalStore } from '@/store/userGlobal';
 
 const store = usePopupStore()
+const dUrl = UrlGlobal()
+const userStore = userGlobalStore()
+
+const usuario = ref<any[]>([]);
+const perfil = ref('')
+onMounted(async () => {
+  const responseO = await axios.get(dUrl.urlGlobal + `/api/seguser/filter?nombre_usuario=${userStore.globalUser}`)
+  usuario.value = responseO.data
+  perfil.value = usuario.value[0]?.perfil
+})
+
+const programas = ref<any[]>([]);
+const cargarProgramas = async (perfil: string) => {
+  try {
+    const response = await axios.post(dUrl.urlGlobal + "/api/procesos_subprogramas", {
+      perfil: perfil,
+    });
+    programas.value = response.data;
+    console.log(programas.value)
+  } catch (error) {
+    console.error("Error cargando programas:", error);
+  }
+};
+
+
 </script>
 
 <style scoped>
